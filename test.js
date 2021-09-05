ff6ae421d3792818ed6c27d565eb3ef2cf138950
dtinth on MBP M1 <dtinth@spacet.me>
2021-09-05 14:40:30 +0700

    Add test

--- test.js
+++ test.js
@@ -5,9 +5,28 @@ const tester = new ShellTester({
 })
 
 tester.session('example', async (s) => {
+  await s.resize(80, 12)
+
   await s.expect('$')
   await s.send('ls\r')
   await s.expect('package.json')
+
+  await s.expect('$')
+
+  // Repeatedly send Ctrl+R until we get a prompt
+  await s.retry(async () => {
+    await s.send('\x12') // Ctrl+R - reverse-i-search
+    await s.expect('reverse-i', 100)
+  }, 3000)
+
+  await s.send('l')
+  await s.expect('ls')
+  await s.capture('reverse-i-search')
+
+  await s.send('\05') // Ctrl+E - go to end-of-line
+  await s.send(' -l\r')
+  await s.expect('drwx')
+  await s.capture('example')
 })
 
 tester.run()
